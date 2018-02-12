9f52f9cb315be4526e615e0bb80b1ecce2db7f6b
Sarah <sarah@github.com>
2018-02-12 15:49:41 -0500

    Added CSS to index.html

--- assignment02/andrew.php
+++ assignment02/andrew.php
@@ -14,7 +14,6 @@
 		<h1>Andrew Regelski</h1>
 	</header>
 
-
 	<nav class="menu" id="t">
 		<ul>
 			<li><a href="index.php">Home</a></li>
@@ -24,53 +23,54 @@
 		</ul>
 	</nav>
 
-	<article>
+	<div class ="border"> 
 
-		<a href="index.php">
+		<article>
 
-				<div class="image-holder">
+			<a href="index.php">
 
-					<img src="images/andrewpic.png" alt="Andrew Regelski ">
+					<div>
 
-				</div>
-			</a>
+						<img src="images/andrewpic.png" alt="Andrew Regelski ">
 
-		<h2>Intiial Team Role: Designer</h2>
-		<p></p>
-		
-		<h3>Skill Set</h3>
+					</div>
+				</a>
 
-        <h3> Executive Summary </h3>
-        <p>
-            I am passionate, highly motivated, well-versed computer science professional eager to take on new challenges in an exciting, fast-paced sphere of mobile and web application design and development.
-        </p>
+			<h2>Intiial Team Role: Designer</h2>
+	
+	        <h3> Executive Summary </h3>
+	        <p>
+	            I am passionate, highly motivated, well-versed computer science professional eager to take on new challenges in an exciting, fast-paced sphere of mobile and web application design and development.
+	        </p>
 
-        <h3> My Projects and Additional Courses </h3>
-        <p>
-            Game Review Web App: users can create accounts, rate and comment on games, and add games (their photo and description) to be reviewed. Comments are sorted by their total like count in descending order. Made with MAMP stack: PHP, MySQL, Python, CSS, HTML, AJAX, Javascript, cookies, and jQuery. Satisfies CRUD (create, read, update, delete) standards. Commenting system implemented via AJAX.  Made independently.
-            Multipage Website: Made with HTML, CSS, JavaScript, and jQuery. Built a website featuring a navigation bar, banner, responsive design. Each page displays information (pic, bio, tables, etc..) about a prominent historical figure. Uses a sitemap and Google Analytics to track data. Search engine optimized. Made independently.
-            Three Games: Made with Android Studio (Java and XML). The main activity contains 3 buttons where each button starts a respective (two-player) game: hotter/colder, hangman, or connect4. Built Follows MVC design pattern. One activity is the main activity. Made independently.
-            Non STEM courses: Public Speaking, Principles of Economics, Psychology of Consumerism, Economic Statistics, Relationships and Emotional Processing, Microeconomics, Social and Emotional Development, and Principles of Marketing.
-        </p>
+	        <h3> My Projects and Additional Courses </h3>
+	        <p>
+	            Game Review Web App: users can create accounts, rate and comment on games, and add games (their photo and description) to be reviewed. Comments are sorted by their total like count in descending order. Made with MAMP stack: PHP, MySQL, Python, CSS, HTML, AJAX, Javascript, cookies, and jQuery. Satisfies CRUD (create, read, update, delete) standards. Commenting system implemented via AJAX.  Made independently.
+	            Multipage Website: Made with HTML, CSS, JavaScript, and jQuery. Built a website featuring a navigation bar, banner, responsive design. Each page displays information (pic, bio, tables, etc..) about a prominent historical figure. Uses a sitemap and Google Analytics to track data. Search engine optimized. Made independently.
+	            Three Games: Made with Android Studio (Java and XML). The main activity contains 3 buttons where each button starts a respective (two-player) game: hotter/colder, hangman, or connect4. Built Follows MVC design pattern. One activity is the main activity. Made independently.
+	            Non STEM courses: Public Speaking, Principles of Economics, Psychology of Consumerism, Economic Statistics, Relationships and Emotional Processing, Microeconomics, Social and Emotional Development, and Principles of Marketing.
+	        </p>
 
-		<h2>Work Experience</h2>
+			<h2>Work Experience</h2>
 
-        <p>
-            Sole website architect. Created and maintained website for a high profile, multi-million dollar networking event so attendees and speakers can register, stay informed, and navigate the event. Job purview: database management, AJAX, PHP, designing and styling webpage, updating website and debugging/solving code problems, monitoring and analyzing weekly website data. E-commerce. 60+ pages. Short timelines, demanding schedule, high-pressure environment. CMS: WordPress.
-            Writing and editing content, designing webpage layout, determining technical requirements, updating websites, creating back up files and solving code problems, database management, AJAX, PHP.
-            Developed and maintained volunteer/work online scheduling system to optimize farm tasks.
-            Answered technical support calls, reviewed and analyzed data, tested products, filed papers, sent faxes, preformed special assignments as instructed.
-        </p>
-
-
-	</article>
+	        <p>
+	            Sole website architect. Created and maintained website for a high profile, multi-million dollar networking event so attendees and speakers can register, stay informed, and navigate the event. Job purview: database management, AJAX, PHP, designing and styling webpage, updating website and debugging/solving code problems, monitoring and analyzing weekly website data. E-commerce. 60+ pages. Short timelines, demanding schedule, high-pressure environment. CMS: WordPress.
+	            Writing and editing content, designing webpage layout, determining technical requirements, updating websites, creating back up files and solving code problems, database management, AJAX, PHP.
+	            Developed and maintained volunteer/work online scheduling system to optimize farm tasks.
+	            Answered technical support calls, reviewed and analyzed data, tested products, filed papers, sent faxes, preformed special assignments as instructed.
+	        </p>
 
 
+		</article>
 
-	<footer>
-		<p> Contact info: aregelski@u.rochester.edu</p>
-		
-	</footer>
+
+
+		<footer>
+			<p> Contact info: aregelski@u.rochester.edu</p>
+			
+		</footer>
+
+	</div> 
 
 </div><!--.container-->
 
