10888da7f6e80c46cdc284c6529c947ba1d03849
Andrew Regelski <aregelsk0113@me.com>
2018-02-12 07:16:09 -0500

    Styled our page, updated nav bar + added my page info

---
+++ assignment02/andrew.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Andrew Regelski</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+
+
+	<header>
+		<h1>Andrew Regelski</h1>
+	</header>
+
+
+	<nav class="menu">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="sarah.php">Information Architect</a></li>
+			<li><a href="patrice.php">Coder</a></li>
+			<li><a href="andrew.php">Designer</a></li>
+		</ul>
+	</nav>
+
+	<article>
+
+		<a href="index.php">
+
+				<div class="image-holder">
+
+					<img src="images/andrewpic.png" alt="Andrew Regelski ">
+
+				</div>
+			</a>
+
+		<h2>Intiial Team Role: Designer</h2>
+		<p></p>
+		
+		<h3>Skill Set</h3>
+
+        <h3> Executive Summary </h3>
+        <p>
+            I am passionate, highly motivated, well-versed computer science professional eager to take on new challenges in an exciting, fast-paced sphere of mobile and web application design and development.
+        </p>
+
+        <h3> My Projects and Additional Courses </h3>
+        <p>
+            Game Review Web App: users can create accounts, rate and comment on games, and add games (their photo and description) to be reviewed. Comments are sorted by their total like count in descending order. Made with MAMP stack: PHP, MySQL, Python, CSS, HTML, AJAX, Javascript, cookies, and jQuery. Satisfies CRUD (create, read, update, delete) standards. Commenting system implemented via AJAX.  Made independently.
+            Multipage Website: Made with HTML, CSS, JavaScript, and jQuery. Built a website featuring a navigation bar, banner, responsive design. Each page displays information (pic, bio, tables, etc..) about a prominent historical figure. Uses a sitemap and Google Analytics to track data. Search engine optimized. Made independently.
+            Three Games: Made with Android Studio (Java and XML). The main activity contains 3 buttons where each button starts a respective (two-player) game: hotter/colder, hangman, or connect4. Built Follows MVC design pattern. One activity is the main activity. Made independently.
+            Non STEM courses: Public Speaking, Principles of Economics, Psychology of Consumerism, Economic Statistics, Relationships and Emotional Processing, Microeconomics, Social and Emotional Development, and Principles of Marketing.
+        </p>
+
+		<h2>Work Experience</h2>
+
+        <p>
+            Sole website architect. Created and maintained website for a high profile, multi-million dollar networking event so attendees and speakers can register, stay informed, and navigate the event. Job purview: database management, AJAX, PHP, designing and styling webpage, updating website and debugging/solving code problems, monitoring and analyzing weekly website data. E-commerce. 60+ pages. Short timelines, demanding schedule, high-pressure environment. CMS: WordPress.
+            Writing and editing content, designing webpage layout, determining technical requirements, updating websites, creating back up files and solving code problems, database management, AJAX, PHP.
+            Developed and maintained volunteer/work online scheduling system to optimize farm tasks.
+            Answered technical support calls, reviewed and analyzed data, tested products, filed papers, sent faxes, preformed special assignments as instructed.
+        </p>
+
+
+	</article>
+
+
+
+	<footer>
+		<p> Contact info: aregelski@u.rochester.edu</p>
+		
+	</footer>
+
+</div><!--.container-->
+
+<script src="http://code.jquery.com/jquery.js"></script>
+
+<script src="js/menu-highlighter.js"></script>
+
+
+</body>
+</html>
